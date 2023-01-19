6a724d142839a765a640b4de2f75ae752bebd23c
Daddy <blacklistuser420@gmail.com>
2023-01-19 18:03:10 +0630

    desrialize error

---
+++ models/users.model.js
@@ -0,0 +1,18 @@
+const Users = require("./users.mongo");
+
+const findUser = async (googleId) => await Users.findOne({ googleId });
+
+const addUser = async (newUser) =>
+  await Users.findOneAndUpdate({ googleId: newUser.googleId }, newUser, {
+    upsert: true,
+    new: true,
+  });
+
+const findById = async (id) => {
+  return await Users.findById(id);
+};
+module.exports = {
+  findUser,
+  addUser,
+  findById,
+};
