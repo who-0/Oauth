a823827dafe0826a9d4800f6f41a33f21ff788e1
Daddy <blacklistuser420@gmail.com>
2023-01-25 18:31:29 +0630

    views modify

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ const github = require("./config/github.passport");
 //!---middleware---
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "..", "views"));
 app.use(bdparser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(cors({ origin: "http://localhost:3000" }));
